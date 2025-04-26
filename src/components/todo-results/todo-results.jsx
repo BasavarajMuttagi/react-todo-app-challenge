bf0805598436ee74160bdcc695f19f703ef08590
BasavarajMuttagi <basavaraj2770@gmail.com>
2025-04-26 12:13:20 +0530

    Fix the count of completed tasks

--- src/components/todo-results/todo-results.jsx
+++ src/components/todo-results/todo-results.jsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
+import { TodosContext } from '../../todo-context';
 import './todo-results.scss';
 
 export const TodoResults = () => {
+  const { todos } = React.useContext(TodosContext);
+
   const calculateChecked = () => {
-    // Fix the app to count the completed tasks
+    const completedTasks = todos.filter((todo) => todo.checked).length;
+    return `${completedTasks} / ${todos.length}`;
   };
 
   return (
     <div className="todo-results">
       Done:
+      {' '}
       {calculateChecked()}
     </div>
   );
